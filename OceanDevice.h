44c211ee323ae126361cf181525752cc36492ed6
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-03-12 03:39:50 +0800

    apple support

--- OceanDevice.h
+++ OceanDevice.h
@@ -103,8 +103,10 @@ class OceanDevice {
   VkQueue presentQueue_;
 
   const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
-  const std::vector<const char *> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME}; 
-  //const std::vector<const char *> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME, "VK_KHR_portability_subset"}; // for macos
+  const std::vector<const char *> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+  #ifdef __APPLE__
+    deviceExtensions.push_back("VK_KHR_portability_subset");
+  #endif
 };
 
 }  // namespace ocean
